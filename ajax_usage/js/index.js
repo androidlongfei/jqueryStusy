9bd1ae3d3b7e26213bcf6e3880b03bc86a4d9237
androidlongfei <androidlongfei@163.com>
2017-06-08 18:01:56 +0800

    ajax

--- ajax_usage/js/index.js
+++ ajax_usage/js/index.js
@@ -1,6 +1,22 @@
 $(document).on('click', '#common', function () {
-    layer.open({
-        content: '移动版和PC版不能同时存在同一页面',
-        btn: '我知道了'
-    });
+    sendAjax()
 })
+
+function sendAjax() {
+    $.ajax({
+        type: 'GET',
+
+        url: 'http://localhost:3800/api/v1/employees ',
+
+        data: '',
+
+        success: function (data) {
+            layer.open({
+                content: '移动版和PC版不能同时存在同一页面' + JSON.stringify(data),
+                btn: '我知道了'
+            });
+        },
+
+        dataType: 'json'
+    })
+}
